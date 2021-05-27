036fa9bd2b67383ef4f402a0c2eeeb72042b0152
Naufal Ramdhan Rizqika <naufalramdhan8@gmail.com>
2021-05-27 14:16:52 +0700

    Modul D: Mencetak Struk

--- AplikasiRestauran/src/classes/Pesanan.java
+++ AplikasiRestauran/src/classes/Pesanan.java
@@ -14,8 +14,9 @@ public class Pesanan {
     private int jumlah;
     private String keterangan;
     
-    public Pesanan(Menu menu, int jumlah){
-        
+     public Pesanan(Menu menu, int jumlah){
+        this.menu = menu;
+        this.jumlah = jumlah;
     }
     
     public void setKeterangan(String keterangan){
@@ -24,5 +25,13 @@ public class Pesanan {
     
     public String getKeterangan(){
         return "";
+    }
+    
+    public Menu getMenu(){
+        return menu;
+    }
+    
+    public int getJumlah(){
+        return jumlah;
     }
 }
--- AplikasiRestauran/src/classes/Transaksi.java
+++ AplikasiRestauran/src/classes/Transaksi.java
@@ -20,22 +20,30 @@ public class Transaksi {
     private double uangBayar;
     private double pajak;
     private double totalBayar;
+    public double biayaService=0;
     
     public Transaksi(String no_transaksi, String nm_pemesan, String tanggal, String no_meja){
         
+        
+         this.noTransaksi = no_transaksi;
+         this.namaPemesan = nm_pemesan;
+         this.tanggal = tanggal;
+         this.noMeja = no_meja;
+         
+         pesanan = new ArrayList<>();
     }
     
     public void tambahPesanan(Pesanan pesanan){
-        
+        this.pesanan.add(pesanan);
     }
     
-    public Pesanan getPesanan(){
-        return null;
-     
-    }
+    //hilangkan dari strukter kelas
+    //public Pesanan getPesanan () [
+    //return pesanan
+    // ]
     
     public ArrayList<Pesanan> getSemuaPesanan(){
-        return null;
+        return pesanan;
     }
     
     public double hitungTotalBayar(){
@@ -45,8 +53,67 @@ public class Transaksi {
     public double hitungKembalian(){
         return 0;
     }
+    public void setBiayaService(double service){
+        this.biayaService = service;
+    }
+    
+    public void setPajak(double pajak){
+        this.pajak = pajak;
+    }
+     public double hitungTotalPesanan(){
+        for(int i = 0; i < pesanan.size(); i++){
+            Pesanan pesan = pesanan.get(i);
+            double harga = pesan.getMenu().getHarga();
+            totalBayar += (harga * pesan.getJumlah());
+            
+        }
+        return totalBayar;
+    }
+    
+    public double hitungPajak(){
+        return totalBayar * pajak;
+    }
+    
+    public double hitungBiayaService(){
+        return totalBayar * biayaService;
+    }
+    
+    public double hitungTotalBayar(double pajak, double service){
+        totalBayar = totalBayar + pajak + service;
+        return totalBayar;
+    }
+    
+    public double hitungKembalian(double uang_bayar){
+        return uang_bayar - totalBayar;
+    }
     
     public void cekStruk(){
+        System.out.println("\n======== RAMEN INDIA ========");
+        System.out.println("No. Transaksi: " + noTransaksi);
+        System.out.println("Pemesan: " + namaPemesan);
+        System.out.println("Tanggal: " + tanggal);
         
+        
+        if(noMeja.equals("")){
+            noMeja = "Take Away";
+        }
+        
+        System.out.println("Meja " + noMeja);
+        System.out.println("==================");
+        for (int i = 0; i < pesanan.size(); i++){
+            Pesanan pesan = pesanan.get(i);
+            Menu m = pesan.getMenu();
+            String pesanan = pesan.getJumlah() + " " + m.getNama_menu() + "\t" +(m.getHarga()*pesan.getJumlah());
+            
+            if(m.getKategori().equals("Kuah")){
+                pesanan = " " + pesanan;
+            }
+            
+            System.out.println(pesanan);
+            
+        }
     }
+    
 }
+    
+
